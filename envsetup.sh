84e60d8a5d3a5df225b87c412e52f3a376545db6
Twisted <twistedumbrella@gmail.com>
2012-01-24 20:03:10 -0500

    build: Support nam configuration script in ota package
    Similar to the --backup command that allows adding the backup and
    restore script, this allows the use of --configid to append the
    modelid_cfg script below the backuptool, as previously configured in
    CM7 builds.
    This adds support for devices that originate from other countries, but
    have a North American counterpart that requires alternate GPS and sound
    profiles.
    Unlike backup tool, which uses the cm prefix to an existing lunch combo
    variable, this flag can apply to devices not currently supported by CM.
    The supported devices are manufactured by HTC, so instead of verifying
    a cm prefix, the htc_ component is used to trigger the append.
    
    Conflicts:
    	core/Makefile
    	envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -55,6 +55,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "htc_") ; then
+        NAM_VARIANT=$(echo -n $1)
+    else
+        NAM_VARIANT=
+    fi
+    export NAM_VARIANT
+
     CALLED_FROM_SETUP=true BUILD_SYSTEM=build/core \
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
