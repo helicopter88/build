07a676dc720c76a473eae7332588e8175ff87763
synergydev <synergy@spentlemon.com>
2013-06-22 15:33:11 +0000

    Revert "am 2e0d1af7: Merge changes I58d43128,Ie34c958f"
    This reverts commit 37868329b29ae7c2399860aee1fcf30f6ebd3fa6, reversing
    changes made to 165c67aacd8c6e8e6ffe57e673602952e4b56219.
    
    Conflicts:
    	core/Makefile
    	target/product/core.mk

--- target/product/core.mk
+++ target/product/core.mk
@@ -171,5 +171,14 @@ ifeq ($(HAVE_SELINUX),true)
         mac_permissions.xml
 endif
 
+ifeq ($(HAVE_SELINUX),true)
+    PRODUCT_PACKAGES += \
+        sepolicy \
+        file_contexts \
+        seapp_contexts \
+        property_contexts \
+        mac_permissions.xml
+endif
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
