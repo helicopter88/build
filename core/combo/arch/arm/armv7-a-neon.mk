ff7c7875c5690a8b1d85c97b741122174317c822
synergydev <synergy@spentlemon.com>
2013-07-02 08:55:01 +0000

    armv7-a: improve defaults

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -1,19 +1,51 @@
 # Configuration for Linux on ARM.
 # Generating binaries for the ARMv7-a architecture and higher with NEON
 #
+TARGET_ARCH_VARIANT_FPU := neon
+
+ARCH_ARM_HAVE_THUMB_SUPPORT     := true
+ARCH_ARM_HAVE_FAST_INTERWORKING := true
+ARCH_ARM_HAVE_64BIT_DATA        := true
+ARCH_ARM_HAVE_HALFWORD_MULTIPLY := true
+ARCH_ARM_HAVE_CLZ               := true
+ARCH_ARM_HAVE_FFS               := true
 ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_TLS_REGISTER      := true
 ARCH_ARM_HAVE_VFP               := true
 ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
-# Note: Hard coding the 'tune' value here is probably not ideal,
-# and a better solution should be found in the future.
-#
 arch_variant_cflags := \
     -march=armv7-a \
     -mfloat-abi=softfp \
     -mfpu=neon
+
+mcpu-arg = $(shell sed 's/^-mcpu=//' <<< "$(call cc-option,-mcpu=$(1),-mcpu=$(2))")
+
+ifeq ($(TARGET_CPU_VARIANT), cortex-a15)
+TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a15,cortex-a9)
+ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
+ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
+#ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
+endif
+ifeq ($(TARGET_CPU_VARIANT), krait)
+TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a9,cortex-a8)
+ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
+ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
+ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
+endif
+ifeq ($(TARGET_CPU_VARIANT), cortex-a9)
+TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a9,cortex-a8)
+ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
+ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
+ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
+endif
+ifeq ($(TARGET_CPU_VARIANT), cortex-a8)
+TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a8,)
+ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
+ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
+ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
+endif
 
 ifneq (,$(findstring cpu=cortex-a9,$(TARGET_EXTRA_CFLAGS)))
 arch_variant_ldflags := \
--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -5,13 +5,37 @@ ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_TLS_REGISTER      := true
 ARCH_ARM_HAVE_VFP               := true
 
-# Note: Hard coding the 'tune' value here is probably not ideal,
-# and a better solution should be found in the future.
-#
 arch_variant_cflags := \
     -march=armv7-a \
     -mfloat-abi=softfp \
     -mfpu=vfpv3-d16
+
+mcpu-arg = $(shell sed 's/^-mcpu=//' <<< "$(call cc-option,-mcpu=$(1),-mcpu=$(2))")
+
+ifeq ($(TARGET_CPU_VARIANT), cortex-a15)
+TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a15,cortex-a9)
+ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
+ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
+#ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
+endif
+ifeq ($(TARGET_CPU_VARIANT), krait)
+TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a9,cortex-a8)
+ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
+ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
+ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
+endif
+ifeq ($(TARGET_CPU_VARIANT), cortex-a9)
+TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a9,cortex-a8)
+ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
+ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
+ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
+endif
+ifeq ($(TARGET_CPU_VARIANT), cortex-a8)
+TARGET_CPU_VARIANT := $(call mcpu-arg,cortex-a8,)
+ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
+ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
+ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
+endif
 
 ifneq (,$(findstring cpu=cortex-a9,$(TARGET_EXTRA_CFLAGS)))
 arch_variant_ldflags := \
